46b0f76214ebbf1843e413484268c90f8fd56e97
Tadeas Firich <tadeasfirich696@gmail.com>
2018-01-10 16:59:16 +0100

    adding in route007 works perfect

--- uu_appg01_trainee-client/src/route/route007.js
+++ uu_appg01_trainee-client/src/route/route007.js
@@ -3,6 +3,7 @@ import createReactClass from "create-react-class";
 import UU5 from "uu5g04";
 import "uu5g04-bricks";
 
+import Counter from "../bricks/route003/counter.js";
 
 export default createReactClass({
 
@@ -40,21 +41,34 @@ export default createReactClass({
   //@@viewOff:overridingMethods
 
   //@@viewOn:componentSpecificHelpers
-
+  _addRow() {
+    let id = UU5.Common.Tools.generateUUID();
+    // díky CcrReader mixinu use můžeme ptát globálního registru na komponentu
+    let counter = this.getCcrComponentByKey("uu5TraineeCount");
+    // zvýšíme číslo v čítači
+    counter.increment();
+    // a přidáme sloupec. Součástí sloupce je i callback metoda v onDelete, který nám zaručí jeho smazání.
+    this._row.insertChild(
+      <h1>
+        <UU5.Bricks.Label colorSchema='info' content='Some task' action="disabled" id={id} />
+      </h1>
+    )
+  },
   //@@viewOff:componentSpecificHelpers
 
   //@@viewOn:render
   render() {
     return (
-      <UU5.Bricks.Div {...this.getMainPropsToPass()} >
+      <UU5.Bricks.Div {...this.getMainPropsToPass()}>
         <UU5.Bricks.Button
           colorSchema="success"
-        >New</UU5.Bricks.Button>
-        <UU5.Bricks.Div displayBlock={true}>
-          <h1>
-            <UU5.Bricks.Label colorSchema='info' content='Beginner'/>
-          </h1>
-        </UU5.Bricks.Div>
+          onClick={this._addRow}
+        />
+        <Counter ccrKey="uu5TraineeCount" />
+        <UU5.Bricks.Row
+          ref_={(cmp) => this._row = cmp}
+          dynamic
+        />
       </UU5.Bricks.Div>
     );
   }
