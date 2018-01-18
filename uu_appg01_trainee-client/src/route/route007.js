e91708022e1d89c307d9f9196f775a4e1bfc0218
Tadeas Firich <tadeasfirich696@gmail.com>
2018-01-18 17:23:46 +0100

    solve proble with new version

--- uu_appg01_trainee-client/src/route/route007.js
+++ uu_appg01_trainee-client/src/route/route007.js
@@ -32,6 +32,11 @@ export default createReactClass({
   //@@viewOff:getDefaultProps
 
   //@@viewOn:standardComponentLifeCycle
+  getInitialState(){
+    return {
+      data:""
+    }
+  },
   //@@viewOff:standardComponentLifeCycle
 
   //@@viewOn:interface
@@ -41,15 +46,30 @@ export default createReactClass({
   //@@viewOff:overridingMethods
 
   //@@viewOn:componentSpecificHelpers
-  _addColumn() {
+  //TODO: dodelat
+  handleChange(values) {
+    this.setState({data: values.name});
+  },
+
+  _addColumn(values) {
     let id = UU5.Common.Tools.generateUUID();
+    console.log(id);
     // díky CcrReader mixinu use můžeme ptát globálního registru na komponentu
     // a přidáme sloupec. Součástí sloupce je i callback metoda v onDelete, který nám zaručí jeho smazání.
     this._row.insertChild(
       <UU5.Bricks.Column colWidth="xs12" style="backgroundColor: grey;margin: 10px 0"  id={id}>
-        <UU5.Bricks.P>xs12</UU5.Bricks.P>
+        <UU5.Bricks.P>{values.name}</UU5.Bricks.P>
+        <UU5.Bricks.Button
+          colorSchema="danger"
+          onClick={() => this._row.deleteChild(id)}
+          content="Delete Column"
+        />
       </UU5.Bricks.Column>
     )
+  },
+  _onSave(component, values) {
+    this._addColumn(values);
+    return this;
   },
   //@@viewOff:componentSpecificHelpers
 
@@ -57,16 +77,35 @@ export default createReactClass({
   render() {
     return (
       <UU5.Bricks.Div {...this.getMainPropsToPass()}>
-        <UU5.Forms.Text
-          label="Full name"
-          placeholder="John Smith"
-          required
-          ref_={input => this._input = input}
-        />
-        <UU5.Bricks.Button
-          colorSchema="success"
-          onClick={this._addColumn}
-        />
+        <UU5.Forms.Form
+          onSave={({component, values}) => this._onSave(component, values)}
+          onCancel={({component}) => this._onCancel(component)}
+        >
+          <UU5.Forms.Text
+            label='Name'
+            name='name'
+            placeholder='Paprika'
+            required
+          />
+          <UU5.Forms.Select
+            label="Category"
+            required
+          >
+            <UU5.Forms.Select.Option value="Maso"/>
+            <UU5.Forms.Select.Option value="Mléčné výrobky"/>
+            <UU5.Forms.Select.Option value="Konzervy"/>
+          </UU5.Forms.Select>
+          <UU5.Forms.Number
+            controlled={true}
+            label="Number of items"
+            value={1}
+          />
+          <UU5.Forms.TextArea
+            label='Description'
+            placeholder='Type here some description'
+          />
+          <UU5.Forms.Controls buttonReset />
+        </UU5.Forms.Form>
         <UU5.Bricks.Row
           ref_={(cmp) => this._row = cmp}
           dynamic
